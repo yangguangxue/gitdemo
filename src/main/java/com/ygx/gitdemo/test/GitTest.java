210d9aed2de9dbde9fae2020f1c5179fcf2fca2b
yangguangxue <15675453227@163.com>
2020-07-08 11:10:34 +0800

    开发01

--- src/main/java/com/ygx/gitdemo/test/GitTest.java
+++ src/main/java/com/ygx/gitdemo/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("this is a class for gittest!!");
         System.out.println("项目经理提交");
         System.out.println("开发人员01");
+        System.out.println("1111111111");
     }
 }
