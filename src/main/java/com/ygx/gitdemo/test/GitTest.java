892e903777617bb20c8321217c4130251010fa6c
yangguangxue <15675453227@163.com>
2020-07-08 10:31:13 +0800

    项目经理 提交

--- src/main/java/com/ygx/gitdemo/test/GitTest.java
+++ src/main/java/com/ygx/gitdemo/test/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is a class for gittest!!");
         System.out.println("项目经理提交");
-        System.out.println("开发01");
+        System.out.println("项目经理01");
     }
 }
