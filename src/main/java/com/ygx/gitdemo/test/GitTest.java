106b8505eb1ac5d92a78ccc3cddcf65f3ead72db
yangguangxue <15675453227@163.com>
2020-07-08 10:28:07 +0800

    master 提交

--- src/main/java/com/ygx/gitdemo/test/GitTest.java
+++ src/main/java/com/ygx/gitdemo/test/GitTest.java
@@ -3,5 +3,6 @@ package com.ygx.gitdemo.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is a class for gittest!!");
+        System.out.println("项目经理提交");
     }
 }
